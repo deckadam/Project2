226574187db1f4ae748970c518a4559b0833bec7
metehancetingybe <metehan.cetin@gybegames.com>
2023-11-25 20:29:07 +0300

    Update input manager to track according to game state

--- Assets/Scripts/InputSystem/InputManager.cs
+++ Assets/Scripts/InputSystem/InputManager.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using Event;
 using Player.Events;
 using UnityEngine;
@@ -6,11 +8,43 @@ namespace InputSystem
 {
     public class InputManager : MonoBehaviour
     {
-        private void Update()
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private void OnEnable()
         {
-            if (Input.GetMouseButtonDown(0))
+            EventSystem.Subscribe<GameStartRequestedEvent>(CheckForInput);
+            EventSystem.Subscribe<FinishLineReachedEvent>(StopCheckingForInput);
+        }
+
+        private void OnDisable()
+        {
+            EventSystem.Unsubscribe<GameStartRequestedEvent>(CheckForInput);
+            EventSystem.Unsubscribe<FinishLineReachedEvent>(StopCheckingForInput);
+        }
+
+        private void StopCheckingForInput(object obj)
+        {
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource?.Dispose();
+            _cancellationTokenSource = null;
+        }
+
+        private async void CheckForInput(object data)
+        {
+            var manulaToken = new CancellationTokenSource();
+            _cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(manulaToken.Token, gameObject.GetCancellationTokenOnDestroy());
+            while (!_cancellationTokenSource.IsCancellationRequested)
             {
-                EventSystem.Raise(new BlockPlaceRequestedEvent());
+                if (Input.GetMouseButtonDown(0))
+                {
+                    EventSystem.Raise(new BlockPlaceRequestedEvent());
+                }
+
+                var isCanceled = await UniTask.NextFrame(cancellationToken: _cancellationTokenSource.Token).SuppressCancellationThrow();
+                if (isCanceled)
+                {
+                    return;
+                }
             }
         }
     }
